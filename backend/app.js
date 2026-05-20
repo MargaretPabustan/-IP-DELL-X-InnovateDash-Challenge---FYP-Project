e7cc1992900b286ba2a73b7641f394dabcb28160
24020804-MargaretPabustan <24020804@myrp.edu.sg>
2026-05-20 19:03:48 +0800

    Implemented basic backend Node.js routing for Sales Teams

--- backend/app.js
+++ backend/app.js
@@ -1,12 +1,9 @@
 // Import required modules for the FYP project //
 const express = require('express');
 const mysql = require('mysql2');
-const cors = require('cors');
-
 const app = express();
 
 app.use(express.json());
-app.use(cors());
 
 const connection = mysql.createConnection({
     host: '5-aozg.h.filess.io',
@@ -24,5 +21,67 @@ connection.connect((err) => {
     console.log('Connected to MySQL database');
 });
 
+// Basic Routes//
+//teams routes//
+// GET all teams
+app.get("/teams", (req, res) => {
+    db.query("SELECT * FROM teams", (err, results) => {
+        if (err) {
+            console.error("Error fetching teams:", err);
+            return res.status(500).json({
+                message: "Error retrieving teams from database"
+            });
+        }
+
+        res.status(200).json(results);
+    });
+});
+
+// GET a specific team by their id//
+app.get("/teams/:id", (req, res) => {
+    db.query(
+        "SELECT * FROM teams WHERE team_id = ?",
+        [req.params.id],
+        (err, results) => {
+            if (err) {
+                console.error("Error fetching team:", err);
+                return res.status(500).json({
+                    message: "Error retrieving team from database"
+                });
+            }
+
+            if (results.length === 0) {
+                return res.status(404).json({
+                    message: "Team not found"
+                });
+            }
+
+            res.status(200).json(results[0]);
+        }
+    );
+});
+
+
+// CREATE team
+app.post("/teams", (req, res) => {
+    const { team_name, territory, description } = req.body;
+
+    db.query(
+        "INSERT INTO teams (team_name, territory, description) VALUES (?, ?, ?)",
+        [team_name, territory, description],
+        (err, result) => {
+            if (err) {
+                console.error("Error creating team:", err);
+                return res.status(500).json({
+                    message: "Error creating team"
+                });
+            }
+
+            res.status(200).json({
+                message: "Team created successfully"
+            });
+        }
+    );
+});
 
 
